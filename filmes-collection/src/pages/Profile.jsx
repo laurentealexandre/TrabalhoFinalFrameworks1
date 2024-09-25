6fcb5519753a7ae8b2f0965a2b3708cde71af772
Alexandre Laurente Cardoso <laurentealexandre@gmail.com>
2024-09-25 15:14:14 -0300

    alterando idioma

--- filmes-collection/src/pages/Profile.jsx
+++ filmes-collection/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../utils/api';
-import './Profile.css'; 
+import styled from 'styled-components';
 
 const Profile = () => {
   const [movie, setMovie] = useState(null);
@@ -10,7 +10,11 @@ const Profile = () => {
   useEffect(() => {
     const fetchMovie = async () => {
       try {
-        const { data } = await api.get(`/movie/${id}`);
+        const { data } = await api.get(`/movie/${id}`, {
+          params: {
+            language: 'pt-BR', // Define o idioma como português do Brasil
+          },
+        });
         setMovie(data);
       } catch (error) {
         console.error('Error fetching movie details:', error);
@@ -24,23 +28,79 @@ const Profile = () => {
   }
 
   return (
-    <div className="profile-container">
-      <h1 className="movie-title">{movie.title}</h1>
-      <div className="movie-details">
-        <img
+    <ProfileContainer>
+      <MovieTitle>{movie.title}</MovieTitle>
+      <MovieDetails>
+        <MoviePoster
           src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
           alt={movie.title}
-          className="movie-poster"
         />
-        <div className="movie-info">
+        <MovieInfo>
           <p><strong>Data de lançamento:</strong> {movie.release_date}</p>
           <p><strong>Avaliação:</strong> {movie.vote_average}/10</p>
           <p><strong>Sinopse:</strong> {movie.overview}</p>
           <p><strong>Gênero:</strong> {movie.genres.map(genre => genre.name).join(', ')}</p>
-        </div>
-      </div>
-    </div>
+        </MovieInfo>
+      </MovieDetails>
+    </ProfileContainer>
   );
 };
 
 export default Profile;
+
+// Styled Components
+
+const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px;
+`;
+
+const MovieTitle = styled.h1`
+  font-size: 2em;
+  margin-bottom: 20px;
+  color: #ffd700;
+`;
+
+const MovieDetails = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 900px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const MoviePoster = styled.img`
+  max-width: 300px;
+  width: 100%;
+  height: auto;
+  margin-right: 20px;
+
+  @media (max-width: 768px) {
+    margin-right: 0;
+    margin-bottom: 20px;
+  }
+`;
+
+const MovieInfo = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+
+  p {
+    margin-bottom: 10px;
+    font-size: 1.1em;
+    color: #ffd700;
+  }
+
+  strong {
+    color: #ffd700;
+  }
+`;
