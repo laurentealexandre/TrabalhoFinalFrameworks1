fa0d6639d52f9d82efbbf4573214f0cf4e63f558
Alexandre Laurente Cardoso <laurentealexandre@gmail.com>
2024-09-25 15:04:36 -0300

    detalhes

--- filmes-collection/src/pages/Profile.jsx
+++ filmes-collection/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../utils/api';
+import './Profile.css'; 
 
 const Profile = () => {
   const [movie, setMovie] = useState(null);
