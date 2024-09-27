ae453d388c5a16545b00b78a4bb9c7af91dca15c
Alexandre Laurente Cardoso <laurentealexandre@gmail.com>
2024-09-27 15:41:33 -0300

    ajustes de codigo

--- filmes-collection/src/Routes.jsx
+++ filmes-collection/src/Routes.jsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import MovieDetails from './pages/MovieDetails'
 import Profile from './pages/Profile'
 import Contact from './pages/Contact'  
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/movie/:id" element={<MovieDetails />} />
+      <Route path="/" element={<Home />} />      
       <Route path="/profile/:id" element={<Profile />} />
       <Route path="/contact" element={<Contact />} />  
     </Routes>
